fc10059b97f0e7a2568805225733cb38c07c5875
tcorrell02 <tyler.correll02@gmail.com>
2026-01-18 15:35:12 -0500

    chore: add crxjs and initial manifest json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {crx} from '@crxjs/vite-plugin'
+import manifest from './public/manifest.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    crx({ manifest })
+  ],
   base: './', //Essential for relative paths in production
 })
