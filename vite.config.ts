90d4a4f4f8bb636e5da1799225e69ea9f2f70027
tcorrell02 <tyler.correll02@gmail.com>
2026-01-18 15:23:10 -0500

    chore: set relative path for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', //Essential for relative paths in production
 })
