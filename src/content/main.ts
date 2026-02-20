90273eac8d459fa6c0fefd7189887df01fbcffd6
tcorrell02 <tyler.correll02@gmail.com>
2026-02-20 15:59:44 -0500

    feat(logged out search): ability to filter job search results when logged out of linkedin, added strategy for logged out search using specific selectors from the logged out search DOM

--- src/content/main.ts
+++ src/content/main.ts
@@ -2,7 +2,7 @@ import { JobSifter } from "../core/JobSifter";
 import { SIFTER_CONFIG } from "../config/constants";
 import { SEARCH_STRATEGIES } from "../core/constants";
 
-const goldpan = new JobSifter(SIFTER_CONFIG, SEARCH_STRATEGIES[0]);
+const goldpan = new JobSifter(SIFTER_CONFIG, SEARCH_STRATEGIES[1]);
 
 goldpan.startSifting();
 
--- src/core/constants.ts
+++ src/core/constants.ts
@@ -12,5 +12,17 @@ export const SEARCH_STRATEGIES: SearchStrategy[] = [
         }
 
     },
+    {
+        name: 'LoggedOutSearch',
+        containerSelector: 'ul.jobs-search__results-list',
+        jobCardSelector: 'div[data-entity-urn^="urn:li:jobPosting:"]',
+        observedAttribute: 'data-entity-urn',
+        
+        getJobId: (card: HTMLElement) => {
+            const urn = card.getAttribute('data-entity-urn');
+            if (urn) return urn.split(':').pop() || null;
+            return null;
+        }
+    }
 ];
 
